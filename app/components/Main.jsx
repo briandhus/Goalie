405921dc57a2571d1209c02ab3b0d5c8de4edfb9
HQ <Spawn@ROPERs-MacBook-Pro.local>
2017-08-21 23:02:05 -0700

    App folder file restructured

---
+++ app/components/Main.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+// var Header123 = require('./Header123.jsx');
+import Header from './Header.jsx';
+var Main = require('./mainR.jsx');
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render (){  
+    return(
+      <div>
+    	<h1>This is the app component</h1>
+    	<Header test='hey'/>
+      </div>
+   )};
+}
+
+// const Header = (props) => {
+//   return <div>{props.test}</div>;
+// }
+// export default Header;
+
+export default App;
