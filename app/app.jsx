cbf1156c509c27e683b6c8e202b8b8c19df9261b
Eri Nagase <stareri@hotmail.com>
2017-08-22 12:40:23 -0700

    creating navbar in Main

--- app/app.jsx
+++ app/app.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
-import Dashboard from './components/children/Dashboard.jsx';
-
-// render((
-//   <BrowserRouter>
-//      <App /> 
-//   </BrowserRouter>
-// ), document.getElementById("app"));
-
 import  Main from './components/Main.jsx';
 
+//import Dashboard from './components/children/Dashboard.jsx';
+
 // render((
 //   <BrowserRouter>
 //      <App /> 
 //   </BrowserRouter>
 // ), document.getElementById("app"));
 
-render(<Form />, document.getElementById("app"));
+
+// render(<Form />, document.getElementById("app"));
 
 // RM Using below lines for quick testing
 render(<BrowserRouter>
