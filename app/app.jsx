2262cf407de94d30beafb580def8dc5e8828993c
HQ <Spawn@calvisitor-10-105-169-226.calvisitor.1918.berkeley.edu>
2017-08-22 10:17:56 -0700

    removing startR

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
+
 import Dashboard from './components/children/Dashboard.jsx';
 
 // render((
@@ -9,9 +9,9 @@ import Dashboard from './components/children/Dashboard.jsx';
 //      <App /> 
 //   </BrowserRouter>
 // ), document.getElementById("app"));
-=======
+
 import  Main from './components/Main.jsx';
->>>>>>> 85d3081931b59ade9be342088d5a7336858db628
+
 
 // RM Using below lines for quick testing
 render(<BrowserRouter>
