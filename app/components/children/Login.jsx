46dc0023bb9ef7e647278cf470047c9f105932c0
Brain Huston <hbriandrums@gmail.com>
2017-08-22 18:05:45 -0700

    added new task inputs for form, cleaned up alignment. Added Login.jsx file with shell

---
+++ app/components/children/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+
+
+class Form extends React.Component {
+
+  constructor(props){
+    super(props);
+    
+  }
+
+}
+
+export default Login;
