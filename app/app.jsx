abf7f659c1ca03c2e7fd0084e11caf7e655dd007
HQ <Spawn@calvisitor-10-105-169-224.calvisitor.1918.berkeley.edu>
2017-08-23 11:29:25 -0700

    merging

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import  Main from './components/Main.jsx';
+import Dashboard from './components/children/Dashboard.jsx';
 
-render(<BrowserRouter>
-      	<Main /> 
-	  </BrowserRouter>, document.getElementById("app"));
+// render(<BrowserRouter>
+//       	<Main /> 
+// 	  </BrowserRouter>, document.getElementById("app"));
+
+render(<Dashboard />, document.getElementById("app"));
