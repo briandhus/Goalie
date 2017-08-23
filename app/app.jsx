75b45eeb811de00232a869210fad07423b145253
HQ <Spawn@ROPERs-MacBook-Pro.local>
2017-08-23 12:59:35 -0700

    merging

--- app/app.jsx
+++ app/app.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import  Main from './components/Main.jsx';
-import Dashboard from './components/children/Dashboard.jsx';
 
-// render(<BrowserRouter>
-//       	<Main /> 
-// 	  </BrowserRouter>, document.getElementById("app"));
-
-render(<Dashboard />, document.getElementById("app"));
+render(<BrowserRouter>
+      	<Main /> 
+	  </BrowserRouter>, document.getElementById("app"));
