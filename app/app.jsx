586c763b0dc402564244e75a7121eabe4431b8a6
Brain Huston <hbriandrums@gmail.com>
2017-08-23 15:15:02 -0700

    adding states to form

--- app/app.jsx
+++ app/app.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import  Main from './components/Main.jsx';
-import ReactRoutes from './react-controller/react-routes';
 
-render(<BrowserRouter>
-      	<Main /> 
-	  </BrowserRouter>, document.getElementById("app"));
+import Form from './components/children/Form.jsx';
+
+render(<Form />, document.getElementById('app'));
+
+// render(<BrowserRouter>
+//       	<Main /> 
+// 	  </BrowserRouter>, document.getElementById("app"));
--- app/components/children/form.jsx
+++ app/components/children/form.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-
-
 
 class Form extends React.Component {
 
   constructor(props){
     super(props);
     
-    this.state = {value: ''};
+    this.state = {goal: {goalName: '', goalDate: ''}, {task: [{taskName: '', taskDate: ''}]};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,7 +20,6 @@ class Form extends React.Component {
     event.preventDefault();
     console.log("CLICK");
     console.log(this.state.value);
-    this.props.setTerm(this.state.value);
     this.setState({ value: "" });
   }
 
@@ -46,7 +42,7 @@ class Form extends React.Component {
                 <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Goal input"/>
               </div>
               <div className="form-group col-md-3">
-                <input className="form-control" type="date" value="2017-09-19" id="example-date-input"/>
+                <input className="form-control" type="date" value={this.state.value} onChange={this.handleChange} id="example-date-input"/>
               </div>
             </form>
           </div>
