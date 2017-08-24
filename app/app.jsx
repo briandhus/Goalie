489e81a10a2744710ecd370b494eb7855198c9bf
Eri Nagase <stareri@hotmail.com>
2017-08-24 09:08:08 -0700

    updating routes

--- app/app.jsx
+++ app/app.jsx
@@ -4,9 +4,9 @@ import { render } from 'react-dom';
 // import { BrowserRouter } from 'react-router-dom';
 
 import { BrowserRouter } from 'react-router-dom';
-import  Main from './components/Main.jsx';
+// import  Main from './components/Main.jsx';
 
-// import routes from './react-controller/react-routes.jsx';
+import routes from './react-controller/react-routes.jsx';
 
 // import Form from './components/children/Form.jsx';
 
