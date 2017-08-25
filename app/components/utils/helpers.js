19f4eef9d07b4a874c6f555cb699448613e667ca
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-08-25 11:14:28 -0700

    modifying heplers and calendar in progress

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import auth from '../../../../config/auth.js'
 
 const helper = {
   // This function hits our own server to update the goal and tasks initially 
@@ -23,7 +24,33 @@ const helper = {
     return axios.get('/api/user/' + username);
   },
 
-  googCalPush: (goal, date) => {
+  googCalPush: (goal, tasks) => {
+    var YOUR_CLIENT_ID = auth.clientID || process.env.GOOGLE_CLIENT_ID;
+    var YOUR_REDIRECT_URI = 'https://goalie-teamgb.herokuapp.com/auth/google/callback' || 'http://localhost:3000/auth/google/callback';
+    var queryString = location.hash.substring(1);
+
+    // Parse query string to see if page request is coming from OAuth 2.0 server.
+    var params = {};
+    var regex = /([^&=]+)=([^&]*)/g, m;
+    while (m = regex.exec(queryString)) {
+      params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+      // Try to exchange the param values for an access token.
+      exchangeOAuth2Token(params);
+    }
+
+  function calAPI() {
+    var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
+    if (params && params['access_token']) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST',
+          'https://www.googleapis.com/drive/v3/about?fields=user&' +
+          'access_token=' + params['access_token']);
+      xhr.onreadystatechange = function (e) {
+        console.log(xhr.response);
+      };
+      xhr.send(null
+    console.log(`Goal: ${goal}`)
+    console.log(`Tasks: ${tasks}`)
     console.log(`pushing to google calendar`)
     // define goal deadline as a calendar event
     const event = {
@@ -49,7 +76,7 @@ const helper = {
       console.log('Event created')
     })
     // create calendar events for tasks
-    const tasks = goal.task
+    // const tasks = goal.task
     for (var i = 0; i < tasks.length; i++) {
       const task = {
         'summary': tasks[i].taskName,
