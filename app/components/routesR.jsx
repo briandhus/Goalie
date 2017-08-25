931ea8134cd8edf85046d28caed5d54f089e14b1
Brain Huston <hbriandrums@gmail.com>
2017-08-25 11:17:34 -0700

    merge

--- app/components/routesR.jsx
+++ app/components/routesR.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
 import LoginOrStart from './children/LoginOrStart.jsx';
@@ -10,70 +10,30 @@ import Dashboard from './children/Dashboard.jsx';
 
 class Routes extends React.Component {
   constructor(props){
-    super(props);
+    super(props)
     this.state= {
-      userLogged: false,
-      serverResponded: false,
-      username: '', 
-      goal: {}
+      userLogged: false
     }
-    this.componentDidMount = this.componentDidMount.bind(this);
-    this.componentDidUpdate = this.componentDidUpdate.bind(this);
-    this.updateTask = this.updateTask.bind(this);
-    this.createGoal = this.createGoal.bind(this);
-  }
-
-  componentDidMount(){
-    var that = this;  
-    axios.get('/api/loggedin').then((data) =>{
-      that.setState({
-        serverResponded: true,
-        userLogged: data
-      })
-    });
-    axios.get('/api/user').then((foundUser) =>{
-      that.setState({
-        username: foundUser.username,
-        goal: foundUser.goal
-      })
-    });
-    
-  }
-
-  componentDidUpdate(){
-    //TODO:
-  }
-
-  createGoal(newGoal){
-    this.setState({
-      goal: newGoal
-    })
-  }
-
-  updateTask(){
-    //TODO
   }
 
   render(){
     return (
-      <div>
       <Switch>
         <Route exact path="/" component={LoginOrStart}/>
-        <Route path="/about" component={About}/>
+        <Route path="/about" component={About}/>   
 
-        <Route exact path="/form" render={(props) => (
-            <Form {...props}/>
+        <Route path="/form" render={(props) => (
+            <Form />
           )}/>      
 
         <Route path="/dashboard" render={(props) => (
-            <Dashboard {...props}/>
+            <Dashboard />
           )}/>
-
       </Switch>
-      </div>
     )
   }
 
 }
+
 
 export default Routes;
