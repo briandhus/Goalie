174b6844334a18252b84aa260cbd454e2c8c9a5f
Eri Nagase <stareri@hotmail.com>
2017-08-28 10:43:57 -0700

    styling and logo

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -12,9 +12,11 @@ class Header extends React.Component {
           <nav className="navbar-toggleable-md navbar-light bg-faded">
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div className="navbar">
-                <Link className="navbar-brand" to="/">Motivate</Link>
+                
+                <Link className="navbar_brand" to="/">
+                  <img alt= "image" src="./assets/images/Motivate.png" className="logo" />
+                </Link>
                 <Link className="navbar_about" to="/about">About</Link>
-
                 <Link className="nav-item nav-link navbar_profile" to="/dashboard">Profile</Link>
 
               </div>
