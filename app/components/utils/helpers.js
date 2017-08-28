2f97b6e3dc5d6c364507535d67b93b3eeb5a5565
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-08-28 10:53:19 -0700

    calendar catch

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 // var auth = require('../../../config/auth.js')
-// if (process.env.GOOGLE_CLIENT_ID){
+if (process.env.GOOGLE_CLIENT_ID){
     var clientID = process.env.GOOGLE_CLIENT_ID;
-  // } else {
-    // var configAuth = require('../../../config/auth.js');
-    // var clientID = configAuth.googleAuth.clientID;
-  // }
+  } else {
+    var configAuth = require('../../../config/auth.js');
+    var clientID = configAuth.googleAuth.clientID;
+  }
 
 const helper = {
   // This function hits our own server to update the goal and tasks initially 
@@ -90,29 +90,31 @@ const helper = {
         // create task events from array
         tasks.forEach((task)=> {
           console.log(task)
-          const taskReminder = {
-            'summary': task.taskName,
-            'start': {
-              'date': task.taskDate
-            },
-            'end': {
-              'date': task.taskDate
-            },
-            'reminders': {
-              'useDefault': false,
-              'overrides': [
-                {'method': 'email', 'minutes': 24 * 60},
-                {'method': 'popup', 'minutes': 12 * 60}
-              ]
-            }
-          };
-          const taskRequest = gapi.client.calendar.events.insert({
-            'calendarId': 'primary',
-            'resource' : taskReminder
-          });
-          taskRequest.execute((event)=>{
-            console.log(event)
-          });
+          if (task.taskName !== "" || task.taskDate !== ""){
+            const taskReminder = {
+              'summary': task.taskName,
+              'start': {
+                'date': task.taskDate
+              },
+              'end': {
+                'date': task.taskDate
+              },
+              'reminders': {
+                'useDefault': false,
+                'overrides': [
+                  {'method': 'email', 'minutes': 24 * 60},
+                  {'method': 'popup', 'minutes': 12 * 60}
+                ]
+              }
+            };
+            const taskRequest = gapi.client.calendar.events.insert({
+              'calendarId': 'primary',
+              'resource' : taskReminder
+            });
+            taskRequest.execute((event)=>{
+              console.log(event)
+            });
+          }
         })
         // if user is not signed in or has not authorized the use of their calendar, redirect to sign in
       } else {
