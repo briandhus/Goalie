ec27c4b40c1d429dd0f8a10cbb61b08747071a88
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-08-28 17:01:50 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
           'process.env': {
-            NODE_ENV: '"production"',
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
           }
         })],
     module: {
