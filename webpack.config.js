50bd39f8034896d80a57e1d3ef83c97dcf12aed7
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-08-28 17:20:27 -0700

    prep

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,6 @@ module.exports = {
         filename: "public/bundle.js",
         publicPath: "/"
     },
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-          }
-        })],
     module: {
         loaders: [
             {
